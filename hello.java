39a7ef17ab904830af17f5586d164f9fa6e3d363
SrinjoySur <91840579+SrinjoySur@users.noreply.github.com>
2024-09-07 01:16:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 class hello{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-System.out.print("Enter a no.:");
-int n = sc.nextInt();
-for(int i=0;i<n;i++){
-System.out.println("Hello World!!"+i);
-}
-}
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a no.:");
+    int n = sc.nextInt();
+    for(int i=0;i<n;i++){
+      System.out.println("Hello World!!"+i);
+    }
+  }
 }
