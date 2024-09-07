4abd19cdbd45460a098e6730edf00e1f75b89908
Srinjoy Sur <21052714@kiit.ac.in>
2024-09-07 18:20:25 +0530

    rechanged

--- hello.java
+++ hello.java
@@ -5,11 +5,7 @@ class hello{
     System.out.print("Enter a no.:");
     int n = sc.nextInt();
     for(int i=0;i<n;i++){
-<<<<<<< HEAD
       System.out.println("Hello World!! "+i+" times printed OK");
-=======
-      System.out.println("Hello World!! "+i+" times printed");
->>>>>>> b1f30e94e425503084ad8aa95e5e052d816c5251
     }
   }
 }
